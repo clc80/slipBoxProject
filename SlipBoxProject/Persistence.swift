b8ad691eb00253d9531470723668d950a8e1d73f
Claudia Maciel <clc_80@yahoo.com>
2025-04-12 18:20:36 -0700

    Setup CloudKit

--- SlipBoxProject/Persistence.swift
+++ SlipBoxProject/Persistence.swift
@@ -5,11 +5,161 @@
 //  Created by Claudia Maciel on 4/11/25.
 //
 
+import CloudKit
+import Combine
 import CoreData
 
-struct PersistenceController {
+class PersistenceController: ObservableObject {
     static let shared = PersistenceController()
 
+    let container: NSPersistentCloudKitContainer
+    var subscriptions = Set<AnyCancellable>()
+
+    @Published var syncErrorMessage: String? = nil
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "SlipBoxProject")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            #if DEBUG
+            setupSchemaSync()
+            #endif
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+
+        checkSyncStatus()
+    }
+    // Check the users cloudKit status. Is it too full, something didn't sync right
+    func checkSyncStatus() {
+        NotificationCenter.default.publisher(for: NSPersistentCloudKitContainer.eventChangedNotification)
+            .sink { notification in
+                guard let event = notification.userInfo?[NSPersistentCloudKitContainer.eventNotificationUserInfoKey] as? NSPersistentCloudKitContainer.Event else { return }
+
+                if event.endDate == nil {
+                    print("cloudKit event start")
+                } else {
+                    switch event.type {
+
+                    case .setup:
+                        print("cloudkit event - setup finished")
+                    case .import:
+                        print("cloudkit event - import finished")
+                    case .export:
+                        print("cloudkit event - export finished")
+                    @unknown default:
+                        print("cloudkit event - added anew event type")
+                    }
+
+                    if event.succeeded {
+                        print("cloudkit event - succeded")
+                    } else {
+                        print("cloudkit event - not succeded")
+                    }
+
+//                    if let error = event.error as? CKError {
+//                        print("cloudkit event - error: \(error.localizedDescription)")
+//                        switch error.code {
+//
+////                        case .internalError:
+////                            <#code#>
+////                        case .partialFailure:
+////                            <#code#>
+////                        case .networkUnavailable:
+////                            <#code#>
+////                        case .networkFailure:
+////                            <#code#>
+////                        case .badContainer:
+////                            <#code#>
+////                        case .serviceUnavailable:
+////                            <#code#>
+////                        case .requestRateLimited:
+////                            <#code#>
+////                        case .missingEntitlement:
+////                            <#code#>
+////                        case .notAuthenticated:
+////                            <#code#>
+////                        case .permissionFailure:
+////                            <#code#>
+////                        case .unknownItem:
+////                            <#code#>
+////                        case .invalidArguments:
+////                            <#code#>
+////                        case .resultsTruncated:
+////                            <#code#>
+////                        case .serverRecordChanged:
+////                            <#code#>
+////                        case .serverRejectedRequest:
+////                            <#code#>
+////                        case .assetFileNotFound:
+////                            <#code#>
+////                        case .assetFileModified:
+////                            <#code#>
+////                        case .incompatibleVersion:
+////                            <#code#>
+////                        case .constraintViolation:
+////                            <#code#>
+////                        case .operationCancelled:
+////                            <#code#>
+////                        case .changeTokenExpired:
+////                            <#code#>
+////                        case .batchRequestFailed:
+////                            <#code#>
+////                        case .zoneBusy:
+////                            <#code#>
+////                        case .badDatabase:
+////                            <#code#>
+//                        case .quotaExceeded:
+//                            self.syncErrorMessage = "quota exceeded, please free up some space on your iCloud"
+////                        case .zoneNotFound:
+////                            <#code#>
+////                        case .limitExceeded:
+////                            <#code#>
+////                        case .userDeletedZone:
+////                            <#code#>
+////                        case .tooManyParticipants:
+////                            <#code#>
+////                        case .alreadyShared:
+////                            <#code#>
+////                        case .referenceViolation:
+////                            <#code#>
+////                        case .managedAccountRestricted:
+////                            <#code#>
+////                        case .participantMayNeedVerification:
+////                            <#code#>
+////                        case .serverResponseLost:
+////                            <#code#>
+////                        case .assetNotAvailable:
+////                            <#code#>
+////                        case .accountTemporarilyUnavailable:
+////                            <#code#>
+//                        @unknown default:
+//                            print("finish up the errors!")
+//                        }
+//                    }
+                }
+            }
+            .store(in: &subscriptions)
+    }
+
+    // MARK: - For debug only
+
+    func setupSchemaSync () {
+        let options = NSPersistentCloudKitContainerSchemaInitializationOptions()
+        do {
+            try container.initializeCloudKitSchema(options: options)
+        } catch {
+            print("Cloud kit sync - schema error: \(error)")
+        }
+    }
+
+    // MARK: - SwiftUI preview helper
     @MainActor
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
@@ -18,40 +168,6 @@ struct PersistenceController {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
         }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
         return result
     }()
-
-    let container: NSPersistentCloudKitContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentCloudKitContainer(name: "SlipBoxProject")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
 }
